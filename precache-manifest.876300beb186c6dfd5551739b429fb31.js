f502e4b36b22dcbfad49f09aac320676e39183d4
vikhyat5 <vikhyatmidha5@gmailcom>
2019-09-05 02:53:46 +0530

    Updates

---
+++ precache-manifest.876300beb186c6dfd5551739b429fb31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b94cbcadd42668677b784804ae8cea",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "4aa7955cfa08dc272bfa",
+    "url": "/react-todo/static/css/main.891b12e9.chunk.css"
+  },
+  {
+    "revision": "b4ad387cf6bab667324b",
+    "url": "/react-todo/static/js/2.9283a890.chunk.js"
+  },
+  {
+    "revision": "4aa7955cfa08dc272bfa",
+    "url": "/react-todo/static/js/main.d650b087.chunk.js"
+  },
+  {
+    "revision": "924314404a1f6066076d",
+    "url": "/react-todo/static/js/runtime~main.99f41c43.js"
+  }
+]);
